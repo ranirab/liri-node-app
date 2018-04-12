fc5e4a04a204c75f642e7a7a2983158a728f2f81
Rani Rab <rani@Ranis-MacBook-Pro.local>
2018-04-11 22:03:37 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # liri-node-app
+
+Create LIRI, a command line node app that takes in a command and parameters to bring back data from Spotify, OMdb, and Twitter
---
+++ liri.js
@@ -0,0 +1,91 @@
+var dotenv = require("dotenv").config();
+var keys = require("./keys.js");
+var request = require('request');
+var fs = require("fs");
+var Twitter = require("twitter");
+var Spotify = require('node-spotify-api');
+var client = new Twitter(keys.twitter);
+var spotify = new Spotify(keys.spotify);
+var choice = process.argv[2];
+var query = process.argv[3];
+
+var switchFunction = function(choice) {
+
+switch (choice) {
+  case "spotify-this-song":
+    if (!query) {
+      query = "the sign" && "Ace of Base";
+    };
+    spotify.search({
+      type: 'track',
+      query: query
+    }, function (err, data) {
+      if (err) {
+        return console.log('Error occurred: ' + err);
+      }
+      for (var i = 0; i < data.tracks.items.length; i++) {
+        for (var j = 0; j < data.tracks.items[i].album.artists.length; j++) {
+          console.log("Artist(s):  " + data.tracks.items[i].album.artists[j].name)
+        };
+        console.log("Song's name:  " + data.tracks.items[i].name);
+        console.log("Preview Link:  " + data.tracks.items[i].preview_url);
+        console.log("Album:  " + data.tracks.items[i].album.name);
+
+      }
+    });
+    break;
+  case "my-tweets":
+    var params = {
+      screen_name: 'nodejs'
+    };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+      if (!error) {
+        for (var i = 0; i < tweets[i].length; i++) {
+
+          console.log(tweets[i].created_at);
+        }
+      }
+    });
+
+    break;
+  case "movie-this":
+    if (!query) {
+      query = "Mr. Nobody"
+    };
+    request("http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy", function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        // // * Title of the movie.
+        console.log("Title is: " + JSON.parse(body).Title);
+        // // * Year the movie came out.
+        console.log("Year released was: " + JSON.parse(body).Year);
+        // // * IMDB Rating of the movie.
+        console.log("imdbRating is: " + JSON.parse(body).imdbRating);
+        // //// * Rotten Tomatoes Rating of the movie.
+        console.log("Rating from Rotten Tomatoes: " + JSON.parse(body).Ratings[1].Value);
+        // // * Country where the movie was produced.
+        console.log("Country where movie was produced:  " + JSON.parse(body).Country);
+        // //  * Language of the movie.
+        console.log("Language of movie: " + JSON.parse(body).Language);
+        // //  * Plot of the movie.
+        console.log("Plot of movie is: " + JSON.parse(body).Plot);
+        // //   * Actors in the movie.
+        console.log("Actors are: " + JSON.parse(body).Actors);
+
+      }
+    });
+    break;
+  case "do-what-it-says":
+    fs.readFile("./random.txt", "utf8", function (error, data) {
+      if (error) {
+        return console.log(error);
+      };
+      console.log(data);
+      var dataArr = data.split(",");
+      console.log(dataArr);
+      query = dataArr[1];
+      switchFunction(dataArr[0]);
+    });
+    break;
+  };
+};
+switchFunction(choice);
